3e03e09b87646d4b7d7a9cd4c79baa2c2a9e48bb
zhangjl@codoon.com <codoon@192.168.2.108>
2016-04-21 23:13:42 +0800

    no message

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+STATIC_URL = '/static/'
+
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -55,7 +65,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,14 +127,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-
-import sys
-SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.abspath(os.path.join(SITE_ROOT, '../static'))
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+
+from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.template import RequestContext
+
+def home(request, template_name="index.html"):
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
