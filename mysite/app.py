8dfe5fbdb67a0d7b2c314a139d32ec54632c13d2
zhangjl@codoon.com <codoon@CodoondeMacBook-Pro.local>
2016-04-22 13:42:12 +0800

    no message

--- mysite/app.py
+++ mysite/app.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+#!/usr/bin/env python
+
+# Run this with
+# PYTHONPATH=. DJANGO_SETTINGS_MODULE=testsite.settings testsite/tornado_main.py
+# Serves by default at
+# http://localhost:8080/hello-tornado and
+# http://localhost:8080/hello-django
+
+from tornado.options import options, define, parse_command_line
+import django.core.handlers.wsgi
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.wsgi
-import tornado.httpserver
-from django.core.handlers.wsgi import WSGIHandler
+import sys
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django.setup()
 
-# 设置 Django 设置模块
-_HERE = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(_HERE)
-os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
 
 
 def main(port):
-    wsgi_app = tornado.wsgi.WSGIContainer(WSGIHandler())
 
-    tornado_app = tornado.web.Application(
-        [('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
-        ])
-    server = tornado.httpserver.HTTPServer(tornado_app)
-    server.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
+  parse_command_line()
+  wsgi_app = tornado.wsgi.WSGIContainer(
+    django.core.handlers.wsgi.WSGIHandler())
+  tornado_app = tornado.web.Application(
+    [
+      ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+      ])
+  server = tornado.httpserver.HTTPServer(tornado_app)
+  server.listen(port)
+  tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == '__main__':
